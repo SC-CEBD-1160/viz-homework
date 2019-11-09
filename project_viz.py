c158a42aa9a668c8b0964ef852091f447c29b6bf
Sebastien Church <schurch@groupeproaction.com>
2019-11-09 07:22:01 -0500

    README

--- project_viz.py
+++ project_viz.py
@@ -132,22 +132,22 @@ axes.legend()
 
 plt.savefig('plots/alcalinity_and_Alcohol_same_plot.png', dpi=300)
 
-# os.makedirs('plots/exploration/', exist_ok=True)
+os.makedirs('plots/exploration/', exist_ok=True)
 
 # Another useful dataset exploration technique involves comparing multiple columns of the dataset
 # The enumerate functions will generate pairs of indexes elements
-# for col1_idx, column1 in enumerate(df.columns):
-#     for col2_idx, column2 in enumerate(df.columns):
-#         if col1_idx < col2_idx:
-#             print(f'Generating {column1} to {column2} plot')
-#             fig, axes = plt.subplots(1, 1, figsize=(5, 5))
-#             axes.scatter(df[column1], df[column2], label=f'{column1} to {column2}', color='green', marker='x')
-#             axes.set_title(f'{column1} to {column2}')
-#             axes.set_xlabel(column1)
-#             axes.set_ylabel(column2)
-#             axes.legend()
-#             plt.savefig(f'plots/exploration/wine_{column1}_{column2}_scatter.png', dpi=300)
-#             plt.close(fig)
+for col1_idx, column1 in enumerate(df.columns):
+    for col2_idx, column2 in enumerate(df.columns):
+        if col1_idx < col2_idx:
+            print(f'Generating {column1} to {column2} plot')
+            fig, axes = plt.subplots(1, 1, figsize=(5, 5))
+            axes.scatter(df[column1], df[column2], label=f'{column1} to {column2}', color='green', marker='x')
+            axes.set_title(f'{column1} to {column2}')
+            axes.set_xlabel(column1)
+            axes.set_ylabel(column2)
+            axes.legend()
+            plt.savefig(f'plots/exploration/wine_{column1}_{column2}_scatter.png', dpi=300)
+            plt.close(fig)
 
 
 # seaborn
